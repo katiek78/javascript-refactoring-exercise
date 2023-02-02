7d75e60f751dde6c49b3941a20523c93cd21f9b1
katiek78 <kermodekatie@gmail.com>
2023-02-02 20:10:34 +0000

    Refactored sort function

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,38 +1,26 @@
-//var txr = [];  ////////
-
 export function processTransactions(transActions) {
 
-    let txr = [];
-
-    //if(!transActions
-    //checks is transActions defined ==> returns falsef
-    //! 
-
-    if(!transActions) {
+    if (!transActions) {
         throw new Error("Undefined collection of transactions")
     }
 
     let txCount = {}
 
-    for(const transaction of transActions){
+    for (const transaction of transActions) {
         txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
     }
 
     txCount = sortByAmountThenName(txCount);
-    
+
     // Place them back in array for returning    
-    return Object.keys(txCount).map(key => `${key} ${txCount[key]}`)    
+    return Object.keys(txCount).map(key => `${key} ${txCount[key]}`)
 }
 
 function sortByAmountThenName(txCount) {
-    const sortedKeys = Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo));
-
+    const sortedKeys = Object.keys(txCount).sort((itemOne, itemTwo) => txCount[itemTwo] - txCount[itemOne] || (itemOne > itemTwo ? 1 : -1));
     const sortedResults = {};
-    for(const objectKey of sortedKeys) {
+    for (const objectKey of sortedKeys) {
         sortedResults[objectKey] = txCount[objectKey];
     }
-
     return sortedResults;
 }
-
-//module.exports = processTransactions;
